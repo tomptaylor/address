d195694780fdc27103ef1949bca86237a11ac47a
Tom Taylor <tomptaylor@gmail.com>
2018-09-08 18:23:57 -0600

    Updating address.js

--- address.js
+++ address.js
@@ -3,14 +3,24 @@
 * ?latlng=39.559539,-104.777031
 */
 var fetchit = require('node-fetch');
+var axios = require('axios');
+
 module.exports = function(context, cb) {
   console.log(context.query);
   var theurl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
   theurl += context.query.latlng;
   theurl += '&API=' + context.secrets.MAP_API;
   console.log(theurl);
-  fetchit(theurl)
-  .then(  res => res.json())
-  .then(data => { cb(null, data);})
-  .catch((error) => console.error(error));
+  // fetchit(theurl)
+  // .then(  res => res.json())
+  // .then(data => { cb(null, data);})
+  // .catch((error) => console.error(error));
+//
+axios.get(theurl)
+  .then(function (response) {
+    console.dir(response.data.results[0]);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 };
