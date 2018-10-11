a7cdc16cf926361c722d8af776cca4707de26de3
Tom Taylor <tomptaylor@gmail.com>
2018-10-11 14:51:07 -0600

    Updating address.js

--- address.js
+++ address.js
@@ -10,7 +10,7 @@ var fetchit = require('node-fetch');
 module.exports = function(context, cb) {
   console.log(context.data.location);  
   var theurl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
-theurl = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?f=pjson&featureTypes=&'
+theurl = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?f=pjson&featureTypes=&location='
   theurl += context.body_raw.location;
 //  theurl += '&API=' + context.secrets.MAP_API;
   console.log(theurl);
