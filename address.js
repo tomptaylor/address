baf378206a4ddb64b6104397387b3ba889eee50e
Tom Taylor <tomptaylor@gmail.com>
2018-10-11 14:03:59 -0600

    Updating address.js

--- address.js
+++ address.js
@@ -1,7 +1,7 @@
 /**
 * @param context {WebtaskContext}
 * ?latlng=39.559539,-104.777031
-* ?lnglat=-104.777031,39.559539
+* location=-104.777031,39.559539
 
 */
 var fetchit = require('node-fetch');
@@ -10,7 +10,7 @@ var fetchit = require('node-fetch');
 module.exports = function(context, cb) {
   console.log(context.query);
   var theurl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
-theurl = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?f=pjson&featureTypes=&location='
+theurl = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?f=pjson&featureTypes=&'
   theurl += context.query.latlng;
 //  theurl += '&API=' + context.secrets.MAP_API;
   console.log(theurl);
